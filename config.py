75c87d861dc836888dd2cee3af959820b32d30de
shabane <m.mohamadshabane@gmail.com>
2022-03-20 17:13:26 +0000

    write the basis configs

---
+++ config.py
@@ -0,0 +1,21 @@
+"""config of the site goes here.
+
+read the full doc to make True decisions; https://shabane.github.io/carbon/configs
+"""
+
+# [Theme]
+DIR = "theme/carbon_default"
+BASE = "base.html"
+CLICK_TO_OPEN = "click_to_open.html"
+REDIRECT_ON_OPEN = "redirect.html"
+INDEX = "index.html"
+
+# [Deployment]
+BASE_URL = "https://shabane.github.io/goto"
+PUBLISH_dir = "docs"
+CHARS = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z',
+]
