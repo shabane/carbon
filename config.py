702a03fee7bbb52bf5a6bdb7ff12dfc17eca7726
shabane <m.mohamadshabane@gmail.com>
2022-03-21 00:17:52 +0000

    update(config.py, core.py):
    	core.py:
    		1. add a Namer() func which will generate a random name for url
    
    	config.py:
    		1. add a config variable to use for max length of the generated url.

--- config.py
+++ config.py
@@ -12,10 +12,11 @@ INDEX = "index.html"
 
 # [Deployment]
 BASE_URL = "https://shabane.github.io/goto"
-PUBLISH_dir = "docs"
+PUBLISH_DIR = "docs"
+MAX_URL_LENGTH = 4  # 36 ^ 4 = 1,679,616 possible addresses for each url
 CHARS = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
     'u', 'v', 'w', 'x', 'y', 'z',
-]
+]  # contain 36 chars
--- core.py
+++ core.py
@@ -1,10 +1,30 @@
 import config
-from bs4 import BeautifulSoup as bs
 import re
 import os
+import random
 
 
-def Make(link: str, title: str, desc: str, click: bool = True) -> str:
+def Namer() -> str:
+    """generate a random name for us as file name to for url.
+    name will end with .html
+
+    Returns:
+        str: name of the file
+    """
+
+    file_name = None
+    while not file_name and not os.path.exists(os.path.join(config.DIR, str(file_name))):
+        for i in range(1, random.randint(1, config.MAX_URL_LENGTH + 1)):
+
+            if not file_name:
+                file_name = ''
+
+            file_name += random.choice(config.CHARS)
+
+    return file_name+'.hmlt'
+
+
+def Make(link: str, name: str, title: str, desc: str, click: bool = True) -> str:
     """make the link director
 
     Args:
@@ -26,7 +46,13 @@ def Make(link: str, title: str, desc: str, click: bool = True) -> str:
         fli = re.sub('\{\{.*link.*\}\}', '"' + link + '"', fli)
         fli = re.sub('\{\{.*title.*\}\}', title, fli)
         fli = re.sub('\{\{.*description.*\}\}', desc, fli)
-        print(fli)
-        
-        
-Make('link', 'title', 'name', False)
+
+        if not os.path.exists(config.PUBLISH_DIR):
+            os.mkdir(config.PUBLISH_DIR)
+        # TODO check the name of the file is empy str or not, if so save it as random, if not, save it as the given name
+        with open(os.path.join(config.PUBLISH_DIR, Namer()), 'w') as fliw:
+            fliw.write(fli)
+
+
+# Make('link', 'title', 'name', 'desc', False)
+print(Namer())
