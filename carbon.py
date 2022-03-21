26785d3fbad9be6d8f4977ea7c0549ba742fa911
shabane <m.mohamadshabane@gmail.com>
2022-03-21 14:20:39 +0000

    update(carbon.py):
    	carbon.py:
    		1. make finish the cli for carbon. do check the user input before
    		variable asing.

--- carbon.py
+++ carbon.py
@@ -1,5 +1,39 @@
-link = input('link which you want to shorten: ')
-name = input('name of url(or just pass it blank to random): ')
-click = input('make user click to open the link(y/n)?: ')
+#!/bin/python3
+from core import Make
+import config
+import os
+
+
+# get the link from user
+while True:
+    link = input('insert link which you want to shorten: ')
+    if link:
+        break
+    else:
+        print('fetal: url should not be blank')
+
+# take a name for shorted url from user
+name = input('short name for url(or just pass it blank to random): ')
+
+# chose user should click to open the link or just redirect it
+while True:
+    click = input('make user click to open the link(y/n)?: ')
+    if click == 'y':
+        click = True
+        break
+    elif click == 'n':
+        click = False
+        break
+    else:
+        print('fetal: enter y for yes, and n for no')
+
+# the title for individual link page
 title = input('type the title: ')
-description = input('describe anything before redirection')
+
+# any desctiption for individual link page
+description = input('describe sotmething before redirection: ')
+
+
+# generate the page
+link = Make(link=link, title=title, desc=description, click=click, name=name)
+print(os.path.join(config.BASE_URL, os.path.split(link)[1]))
