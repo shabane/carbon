87607cbae85d9224f59dcf621dbd526f83594b27
shabane <m.mohamadshabane@gmail.com>
2022-03-22 04:53:51 +0430

    make the test better.

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
+"""
+this is a test, run this test if you are developing the theme.
+if you want to be cofortable on the theme developing, just
+use this command. `watch -n 1 'python3 test.py'`
+this command will run the test and make two test html page each one second
+"""
 from core import Make
 import shutil
 
 
 shutil.copy('theme/carbon_default/style.css', 'docs/style.css')
-# Make('', 'title', 'description', name='1')
 
-# Make('https://google.com', 'go to google', """
-# this link is just for fun which direct you to gogle.com     
-# """, name='1')
 desk = """
 hi, if you are seeing this link, you are so lucky to see it.
 well, you should click on the button due to open the actual link :)
 """  # desk ? desc!
 
 Make('https://google.com', 'go to google.com', desc=desk, click=True, name='click')
-Make('https://google.com', 'go to google.com', desc=desk, click=False, name='redirect')
+Make('https://google.com', 'go to google.com', desc='redirect you...', click=False, name='redirect')
